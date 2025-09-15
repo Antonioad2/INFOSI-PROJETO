e58b3290753d3c235a69db5b8eddf8ba04130071
Braulio-Cruz <brauliosn57@gmail.com>
2025-09-15 12:42:40 +0100

    Alterações no front

--- resources/views/site/_parcial/header.blade.php
+++ resources/views/site/_parcial/header.blade.php
@@ -1,139 +1,198 @@
-<header class="header-section">
-                <!-- Offcanvas Area Start -->
-                <div class="fix-area">
-                    <div class="offcanvas__info">
-                        <div class="offcanvas__wrapper">
-                            <div class="offcanvas__content">
-                                <div class="offcanvas__top mb-5 d-flex justify-content-between align-items-center">
-                                    <div class="offcanvas__logo">
-                                        <a href="index.html">
-                                            <img src="{{ url('assets/user/img/logo/black-logo.svg')}}" alt="logo-img">
-                                        </a>
-                                    </div>
-                                    <div class="offcanvas__close">
-                                        <button><i class="fas fa-times"></i></button>
-                                    </div>
-                                </div>
-                                <p class="text d-none d-xl-block">
-                                    Nullam dignissim, ante scelerisque the is euismod fermentum odio sem semper the is erat, a feugiat leo urna eget eros. Duis Aenean a imperdiet risus.
-                                </p>
-                                <div class="mobile-menu fix mb-3"></div>
-                                <div class="offcanvas__contact">
-                                    <h4>Informações de Contato</h4>
-                                    <ul>
-                                        <li class="d-flex align-items-center">
-                                            <div class="offcanvas__contact-icon"><i class="fal fa-map-marker-alt"></i></div>
-                                            <div class="offcanvas__contact-text">
-                                                <a target="_blank" href="#">Rua Principal, Luanda, Angola</a>
-                                            </div>
-                                        </li>
-                                        <li class="d-flex align-items-center">
-                                            <div class="offcanvas__contact-icon mr-15"><i class="fal fa-envelope"></i></div>
-                                            <div class="offcanvas__contact-text">
-                                                <a href="mailto:info@example.com"><span class="mailto:info@example.com">info@example.com</span></a>
-                                            </div>
-                                        </li>
-                                        <li class="d-flex align-items-center">
-                                            <div class="offcanvas__contact-icon mr-15"><i class="fal fa-clock"></i></div>
-                                            <div class="offcanvas__contact-text">
-                                                <a target="_blank" href="#">Segunda-Sexta, 09h-17h</a>
-                                            </div>
-                                        </li>
-                                        <li class="d-flex align-items-center">
-                                            <div class="offcanvas__contact-icon mr-15"><i class="far fa-phone"></i></div>
-                                            <div class="offcanvas__contact-text">
-                                                <a href="tel:+244923456789">+244 923 456 789</a>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                    <div class="header-button mt-4">
-                                        <a href="contact.html" class="theme-btn text-center">
-                                            <span>Solicitar Orçamento<i class="fa-solid fa-arrow-right-long"></i></span>
-                                        </a>
-                                    </div>
-                                    <div class="social-icon d-flex align-items-center">
-                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                        <a href="#"><i class="fab fa-twitter"></i></a>
-                                        <a href="#"><i class="fab fa-youtube"></i></a>
-                                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+<!-- Offcanvas Area Start -->
+<div class="fix-area">
+    <div class="offcanvas__info">
+        <div class="offcanvas__wrapper">
+            <div class="offcanvas__content">
+                <div class="offcanvas__top mb-5 d-flex justify-content-between align-items-center">
+                    <div class="offcanvas__logo">
+                        <a href="index.html">
+                            <img src="assets/user/img/logo/black-logo.svg" alt="logo-img">
+                        </a>
+                    </div>
+                    <div class="offcanvas__close">
+                        <button>
+                            <i class="fas fa-times"></i>
+                        </button>
                     </div>
                 </div>
-    
-            {{-- <div class="header-top-section style-two">
-                <div class="container-fluid">
-                    <div class="header-top-wrapper style-2">
-                        <ul class="contact-list">
-                            <li>
-                                <i class="fas fa-envelope"></i>
-                                <a href="mailto:info@example.com" class="link">infosi@gov.ao</a>
-                            </li>
-                            <li>
-                                <i class="fas fa-map-marker-alt"></i>
-                                Martires. Luanda, Angola
-                            </li>
-                        </ul>
-                        <div class="header-top-right">
-                            <ul class="top-list">
-                                <li><a href="contact.html">Ajuda</a></li>
-                                <li><a href="contact.html">Suporte</a></li>
-                                <li><a href="contact.html">Contacto</a></li>
-                            </ul>
-                            <div class="social-icon d-flex align-items-center">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <p class="text d-none d-xl-block">
+                    Nullam dignissim, ante scelerisque the is euismod fermentum odio sem semper the is erat, a feugiat
+                    leo urna eget eros. Duis Aenean a imperdiet risus.
+                </p>
+                <div class="mobile-menu fix mb-3"></div>
+                <div class="offcanvas__contact">
+                    <h4>Contact Info</h4>
+                    <ul>
+                        <li class="d-flex align-items-center">
+                            <div class="offcanvas__contact-icon">
+                                <i class="fal fa-map-marker-alt"></i>
                             </div>
-                        </div>
+                            <div class="offcanvas__contact-text">
+                                <a target="_blank" href="#">Main Street, Melbourne, Australia</a>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center">
+                            <div class="offcanvas__contact-icon mr-15">
+                                <i class="fal fa-envelope"></i>
+                            </div>
+                            <div class="offcanvas__contact-text">
+                                <a href="mailto:info@example.com"><span
+                                        class="mailto:info@example.com">info@example.com</span></a>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center">
+                            <div class="offcanvas__contact-icon mr-15">
+                                <i class="fal fa-clock"></i>
+                            </div>
+                            <div class="offcanvas__contact-text">
+                                <a target="_blank" href="#">Mod-friday, 09am -05pm</a>
+                            </div>
+                        </li>
+                        <li class="d-flex align-items-center">
+                            <div class="offcanvas__contact-icon mr-15">
+                                <i class="far fa-phone"></i>
+                            </div>
+                            <div class="offcanvas__contact-text">
+                                <a href="tel:+11002345909">+11002345909</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="header-button mt-4">
+                        <a href="contact.html" class="theme-btn text-center">
+                            <span>Get A Quote<i class="fa-solid fa-arrow-right-long"></i></span>
+                        </a>
+                    </div>
+                    <div class="social-icon d-flex align-items-center">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
-            </div> --}}
-            <div id="header-sticky" class="header-3">
-                <div class="container-fluid">
-                    <div class="mega-menu-wrapper">
-                        <div class="header-main">
-                            <div class="header-left">
-                                <div class="logo">
-                                    <a href="{{route('site.home')}}" class="header-logo-1">
-                                        <img src="{{ url('assets/user/img/logo/black-logo.svg')}}" alt="logo-img">
-                                    </a>
-                                    <a href="index.html" class="header-logo-2">
-                                        <img src="{{ url('assets/user/img/logo/black-logo.svg')}}" alt="logo-img">
-                                    </a>
-                                </div>
-                                {{-- <div class="mean__menu-wrapper">
-                                    <div class="main-menu">
-                                    @include('site._parcial.menu')
-                                    </div>
-                                </div> --}}
-                            </div>
-                            <div class="header-right d-flex justify-content-end align-items-center">
-                                <div class="icon-items">
-                                    <div class="menu-items">
-                                        @include('site._parcial.menu')
-                                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="offcanvas__overlay"></div>
 
-                                    <div class="icon">
-                                        <i class="fas fa-phone-alt"></i>
-                                    </div>
-                                    {{-- <div class="content">
-                                        <p>Liga Para Nós</p>
-                                        <h6><a href="tel:+9288009850">+92 (8800) - 9850</a></h6>
-                                    </div> --}}
-                                </div>
-                               {{-- <a href="#0" class="search-trigger search-icon"><i class="fa-regular fa-magnifying-glass"></i></a> --}}
-                                <div class="header__hamburger d-xl-none my-auto" style="background: color:#0c142e">
-                                    <div class="sidebar__toggle">
-                                        <i class="fas fa-bars"></i>
-                                    </div>
-                                </div>
-                            </div>
+<!-- Header Top Section Start -->
+<div class="header-top-section">
+    <div class="container-fluid">
+        <div class="header-top-wrapper">
+            <ul class="contact-list">
+                <li>
+                    <i class="fas fa-envelope"></i>
+                    <a href="mailto:info@example.com" class="link">geral@angocars.com</a>
+                </li>
+                <li>
+                    <i class="fas fa-map-marker-alt"></i>
+                    Mutamba, Luanda Angola
+                </li>
+            </ul>
+            <div class="header-top-right">
+                <ul class="top-list">
+                    <li><a href="contact.html">Conecte-se</a></li>
+                    <li><a href="contact.html">Inscrever-se</a></li>
+                </ul>
+                <div class="social-icon d-flex align-items-center">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Header Section Start -->
+<header id="header-sticky" class="header-1">
+    <div class="container-fluid">
+        <div class="mega-menu-wrapper">
+            <div class="header-main">
+                <div class="header-left">
+                    <div class="logo">
+                        <a href="index.html" class="header-logo">
+                            <img src="assets/user/img/logo/black-logo.svg" alt="logo-img">
+                        </a>
+                    </div>
+                    <div class="mean__menu-wrapper">
+                        <div class="main-menu">
+                            <nav id="mobile-menu">
+                                <ul>
+                                    <li class="has-dropdown active menu-thumb">
+                                        <a href="index.html">
+                                            Início
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="about.html">Carros</a>
+                                    </li>
+                                    <li class="has-dropdown">
+                                        <a href="news.html">
+                                            Serviços
+                                            <i class="fas fa-angle-down"></i>
+                                        </a>
+                                        <ul class="submenu">
+                                            <li><a href="gallery.html">Gallery</a></li>
+                                            <li><a href="faq.html">Faq's</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="program-details.html">
+                                            Blogue
+                                            <i class="fas fa-angle-down"></i>
+                                        </a>
+                                        <ul class="submenu">
+                                            <li><a href="car-grid.html">Car Grid</a></li>
+                                            <li><a href="car-list.html">Car List</a></li>
+                                            <li><a href="car-list-sidebar.html">Car Sidebar</a></li>
+                                            <li><a href="car-details.html">Car Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="news.html">
+                                            Oferta
+                                            <i class="fas fa-angle-down"></i>
+                                        </a>
+                                        <ul class="submenu">
+                                            <li><a href="news.html">Blog</a></li>
+                                            <li><a href="news-details.html">Blog Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">Sobre Nós</a>
+                                            <i class="fas fa-angle-down"></i>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-right d-flex justify-content-end align-items-center">
+                    <div class="icon-items">
+                        <div class="icon">
+                            <i class="fas fa-phone-alt"></i>
+                        </div>
+                        <div class="content">
+                            <p>Call Anytime</p>
+                            <h6><a href="tel:+9288009850">+92 (8800) - 9850</a></h6>
+                        </div>
+                    </div>
+                    <a href="#0" class="search-trigger search-icon"><i
+                            class="fa-regular fa-magnifying-glass"></i></a>
+                    <div class="header-button">
+                        <a href="car-details.html" class="header-btn">
+                            Find a Car
+                        </a>
+                    </div>
+                    <div class="header__hamburger d-xl-none my-auto">
+                        <div class="sidebar__toggle">
+                            <i class="fas fa-bars"></i>
                         </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
+    </div>
+</header>
--- resources/views/site/home/layout/main.blade.php
+++ resources/views/site/home/layout/main.blade.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-    <!-- ========== Meta Tags ========== -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="pixydrops">
-    <meta name="description" content="Remons - Template HTML para Aluguel de Carros">
-    <!-- ======== Título da Página ============ -->
-    <title>Remons - Template HTML para Aluguel de Carros</title>
-    <!--<< Favicon >>-->
-    <link rel="shortcut icon" href="{{ url('assets/user/img/favicon.png')}}">
-    <!--<< Bootstrap min.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/bootstrap.min.css')}}">
-    <!--<< All Min Css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/all.min.css')}}">
-    <!--<< Animate.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/animate.css')}}">
-    <!--<< Magnific Popup.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/magnific-popup.css')}}">
-    <!--<< MeanMenu.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/meanmenu.css')}}">
-    <!--<< DatePicker.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/datepickerboot.css')}}">
-    <!--<< Swiper Bundle.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/swiper-bundle.min.css')}}">
-    <!--<< Nice Select.css>>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/nice-select.css')}}">
-    <!--<< Main.css >>-->
-    <link rel="stylesheet" href="{{ url('assets/user/css/main.css')}}">
-    <link rel="stylesheet" href="{{ url('assets/user/css/home.css')}}">
-    
+        <!-- ========== Meta Tags ========== -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="pixydrops">
+        <meta name="description" content="Remons - Booking Rental HTML Template">
+        <!-- ======== Page title ============ -->
+        <title>Remons - Booking Rental HTML Template</title>
+        <!--<< Favcion >>-->
+        <link rel="shortcut icon" href="assets/user/img/favicon.png">
+        <!--<< Bootstrap min.css >>-->
+        <link rel="stylesheet" href="assets/user/css/bootstrap.min.css">
+        <!--<< All Min Css >>-->
+        <link rel="stylesheet" href="assets/user/css/all.min.css">
+        <!--<< Animate.css >>-->
+        <link rel="stylesheet" href="assets/user/css/animate.css">
+        <!--<< Magnific Popup.css >>-->
+        <link rel="stylesheet" href="assets/user/css/magnific-popup.css">
+        <!--<< MeanMenu.css >>-->
+        <link rel="stylesheet" href="assets/user/css/meanmenu.css">
+        <!--<< DatePicker.css >>-->
+        <link rel="stylesheet" href="assets/user/css/datepickerboot.css">
+        <!--<< Swiper Bundle.css >>-->
+        <link rel="stylesheet" href="assets/user/css/swiper-bundle.min.css">
+        <!--<< Nice Select.css >>-->
+        <link rel="stylesheet" href="assets/user/css/nice-select.css">
+        <!--<< Main.css >>-->
+        {{-- <link rel="stylesheet" href="assets/user/css/main.css"> --}}
+
+        <link rel="stylesheet" href="assets/user/css/main2.css">
 </head>
 <body>
 
@@ -40,20 +40,31 @@
     @include('site._parcial.footer')
     </main>
 
-    <!--<< All JS Plugins >>-->
-    <script src="{{ url('assets/user/js/jquery-3.7.1.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/viewport.jquery.js')}}"></script>
-    <script src="{{ url('assets/user/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/jquery.nice-select.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/jquery.waypoints.js')}}"></script>
-    <script src="{{ url('assets/user/js/jquery.counterup.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/bootstrap-datepicker.js')}}"></script>
-    <script src="{{ url('assets/user/js/swiper-bundle.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/jquery.meanmenu.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/jquery.magnific-popup.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/animation.js')}}"></script>
-    <script src="{{ url('assets/user/js/wow.min.js')}}"></script>
-    <script src="{{ url('assets/user/js/main.js')}}"></script>
-    <script src="{{ asset('assets/js/date-handle.js') }}"></script>
+        <!--<< All JS Plugins >>-->
+        <script src="assets/user/js/jquery-3.7.1.min.js"></script>
+        <!--<< Viewport Js >>-->
+        <script src="assets/user/js/viewport.jquery.js"></script>
+        <!--<< Bootstrap Js >>-->
+        <script src="assets/user/js/bootstrap.bundle.min.js"></script>
+        <!--<< Nice Select Js >>-->
+        <script src="assets/user/js/jquery.nice-select.min.js"></script>
+        <!--<< Waypoints Js >>-->
+        <script src="assets/user/js/jquery.waypoints.js"></script>
+        <!--<< Counterup Js >>-->
+        <script src="assets/user/js/jquery.counterup.min.js"></script>
+        <!--<< Datepicker Js >>-->
+        <script src="assets/user/js/bootstrap-datepicker.js"></script>
+        <!--<< Swiper Slider Js >>-->
+        <script src="assets/user/js/swiper-bundle.min.js"></script>
+        <!--<< MeanMenu Js >>-->
+        <script src="assets/user/js/jquery.meanmenu.min.js"></script>
+        <!--<< Magnific Popup Js >>-->
+        <script src="assets/user/js/jquery.magnific-popup.min.js"></script>
+        <!--<< GSAP Animation Js >>-->
+        <script src="assets/user/js/animation.js"></script>
+        <!--<< Wow Animation Js >>-->
+        <script src="assets/user/js/wow.min.js"></script>
+        <!--<< Main.js >>-->
+        <script src="assets/user/js/main.js"></script>
 </body>
 </html>
