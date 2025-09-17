104cc4277fe4d2a5c480148814c5dc430b7cf31f
Braulio-Cruz <brauliosn57@gmail.com>
2025-09-17 09:32:53 +0100

    corte front

---
+++ resources/views/site/layouts/main.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from dreamsrent.dreamstechnologies.com/html/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Sep 2025 11:21:41 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Dreams Rent | Template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ url('assets/user/img/favicon.png') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/css/bootstrap.min.css') }}">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/fontawesome/css/all.min.css') }}">
+
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/select2/css/select2.min.css') }}">
+
+    <!-- Datepicker CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/css/bootstrap-datetimepicker.min.css') }}">
+
+    <!-- Aos CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/aos/aos.css') }}">
+
+    <!-- Fearther CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/css/feather.css') }}">
+
+    <!-- Owl carousel CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/css/owl.carousel.min.css') }}">
+
+    <!-- Flatpickr CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/flatpickr/flatpickr.min.css') }}">
+
+    <!-- Fancybox CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/fancybox/fancybox.css') }}">
+
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/slick/slick.css') }}">
+
+    <!-- Boxicons CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/plugins/boxicons/css/boxicons.min.css') }}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ url('assets/user/css/style.css') }}">
+
+</head>
+
+<body>
+
+    @include('site.layouts.header')
+    <main class="nxl-container">
+        @yield('content')
+        @include('site.layouts.footer')
+    </main>
+
+
+
+
+    <!-- jQuery -->
+    <script src="{{ url('assets/js/jquery-3.7.1.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Bootstrap Core JS -->
+    <script src="{{ url('assets/js/bootstrap.bundle.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- counterup JS -->
+    <script src="{{ url('assets/js/jquery.waypoints.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+    <script src="{{ url('assets/js/jquery.counterup.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Select2 JS -->
+    <script src="{{ url('assets/plugins/select2/js/select2.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Aos -->
+    <script src="{{ url('assets/plugins/aos/aos.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Top JS -->
+    <script src="{{ url('assets/js/backToTop.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Owl Carousel JS -->
+    <script src="{{ url('assets/js/owl.carousel.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Slick JS -->
+    <script src="{{ url('assets/plugins/slick/slick.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Flatpickr JS -->
+    <script src="{{ url('assets/plugins/flatpickr/flatpickr.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+    <script src="{{ url('assets/plugins/flatpickr/forms-pickers.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Datepicker Core JS -->
+    <script src="{{ url('assets/plugins/moment/moment.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+    <script src="{{ url('assets/js/bootstrap-datetimepicker.min.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Fancybox JS -->
+    <script src="{{ url('assets/plugins/fancybox/fancybox.umd.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <!-- Custom JS -->
+    <script src="{{ url('assets/js/script.js')}}" type="705cb7e2d14f5288c24a7639-text/javascript"></script>
+
+    <script src="{{ url('../../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js') }}"
+        data-cf-settings="705cb7e2d14f5288c24a7639-|49" defer></script>
+    <script defer src="../../../static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
+        integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
+        data-cf-beacon='{"rayId":"97fffa7f8ad877d9","version":"2025.8.0","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"token":"3ca157e612a14eccbb30cf6db6691c29","b":1}'
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
