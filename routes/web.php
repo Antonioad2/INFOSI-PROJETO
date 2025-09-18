48540e9b9ef87907db38b3245a90bd72f796791b
Braulio-Cruz <brauliosn57@gmail.com>
2025-09-18 14:55:00 +0100

    corte do car-location 7 car-services

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/car/{car_id}', [HomeController::class, 'carDetails'])->name('car.details');
 Route::get('/car-list', [HomeController::class, 'carList'])->name('site.car-list');
 Route::get('/car/{id}/book', [HomeController::class, 'carBook'])->name('car.book');
+Route::get('/car-location', [HomeController::class, 'carLocation'])->name('site.car-location');
+Route::get('/car-services', [HomeController::class, 'carServices'])->name('site.car-services');
 
