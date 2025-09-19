d05b1925a11a63b3b121490182a797829799520b
Braulio-Cruz <brauliosn57@gmail.com>
2025-09-19 08:20:21 +0100

    merge

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,13 @@ Route::get('/car-list', [HomeController::class, 'carList'])->name('site.car-list
 Route::get('/car/{id}/book', [HomeController::class, 'carBook'])->name('car.book');
 Route::get('/car-location', [HomeController::class, 'carLocation'])->name('site.car-location');
 Route::get('/car-services', [HomeController::class, 'carServices'])->name('site.car-services');
+Route::get('/car-billing', [HomeController::class, 'carBilling'])->name('site.car-billing');
+Route::get('/car-payment', [HomeController::class, 'carPayment'])->name('site.car-payment');
+Route::get('/car-confirmed', [HomeController::class, 'carConfirmed'])->name('site.car-confirmed');
+
 
  Route::get('/car-details', function () {
     return view('site.reservation.car-details.index');
 })->name('car.details');
 
     /* Rota Sobre Nós */
-
-
